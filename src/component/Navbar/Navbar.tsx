7307163dfe21034873748b380bf7f102130db048
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-01 21:55:02 +0900

    created nav bar component adding buttons and logo

---
+++ src/component/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+
+const Navbar = () => {
+	return (
+		<AppBar>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="static">
+					<Toolbar>
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{ flexGrow: 1 }}
+						>
+							Time Tracker
+						</Typography>
+						<Box mx={1}>
+							<Button color="inherit">Today</Button>
+						</Box>
+						<Box mx={1}>
+							<Button color="inherit">Work History</Button>
+						</Box>
+						<Box mx={1}>
+							<Button color="inherit" variant="outlined">
+								Login
+							</Button>
+						</Box>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</AppBar>
+	);
+};
+
+export default Navbar;
