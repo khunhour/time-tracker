41604719c1ef95ac69e16882638d3b92b730cbf3
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-05 14:30:31 +0900

    deleted unused appbar

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -3,32 +3,30 @@ import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
 
 const Navbar = () => {
 	return (
-		<AppBar>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="static">
-					<Toolbar>
-						<Typography
-							variant="h6"
-							component="div"
-							sx={{ flexGrow: 1 }}
-						>
-							Time Tracker
-						</Typography>
-						<Box mx={1}>
-							<Button color="inherit">Today</Button>
-						</Box>
-						<Box mx={1}>
-							<Button color="inherit">Work History</Button>
-						</Box>
-						<Box mx={1}>
-							<Button color="inherit" variant="outlined">
-								Login
-							</Button>
-						</Box>
-					</Toolbar>
-				</AppBar>
-			</Box>
-		</AppBar>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1 }}
+					>
+						Time Tracker
+					</Typography>
+					<Box mx={1}>
+						<Button color="inherit">Today</Button>
+					</Box>
+					<Box mx={1}>
+						<Button color="inherit">Work History</Button>
+					</Box>
+					<Box mx={1}>
+						<Button color="inherit" variant="outlined">
+							Login
+						</Button>
+					</Box>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 };
 
