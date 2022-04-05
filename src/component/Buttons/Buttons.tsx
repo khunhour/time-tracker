334f640c3eb5e95b9c0bcacf860379e75823f8e8
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-05 14:37:47 +0900

    added margin and disable feature

--- src/component/Buttons/Buttons.tsx
+++ src/component/Buttons/Buttons.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Paper } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 
 const Buttons = () => {
@@ -6,16 +6,18 @@ const Buttons = () => {
 		<>
 			<Box
 				mt={9}
+				mb={2}
 				sx={{
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "center",
+					gap: "10px",
 				}}
 			>
-				<Button variant="contained" color="success">
+				<Button variant="contained" color="success" disabled={false}>
 					Start Work
 				</Button>
-				<Button variant="contained" color="error">
+				<Button variant="contained" color="error" disabled={true}>
 					End Work
 				</Button>
 			</Box>
@@ -24,12 +26,13 @@ const Buttons = () => {
 					display: "flex",
 					alignItems: "center",
 					justifyContent: "center",
+					gap: "10px",
 				}}
 			>
-				<Button variant="contained" color="warning">
+				<Button variant="contained" color="warning" disabled={false}>
 					Start Break
 				</Button>
-				<Button variant="contained" color="warning">
+				<Button variant="contained" color="warning" disabled={true}>
 					End Break
 				</Button>
 			</Box>
