f2d6547f4204d316c355199714ce56d64a96f11c
Khunhour Morm <khonhourmorm@gmail.com>
2022-04-07 03:13:28 +0900

    added click handler for start and end work button

--- src/component/Buttons/Buttons.tsx
+++ src/component/Buttons/Buttons.tsx
@@ -1,7 +1,12 @@
 import { Box, Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const Buttons = () => {
+	const [startWork, setStartWork] = useState(false);
+
+	const handleWorkButton = () => {
+		setStartWork(!startWork);
+	};
 	return (
 		<>
 			<Box
@@ -14,10 +19,20 @@ const Buttons = () => {
 					gap: "10px",
 				}}
 			>
-				<Button variant="contained" color="success" disabled={false}>
+				<Button
+					variant="contained"
+					color="success"
+					disabled={startWork}
+					onClick={handleWorkButton}
+				>
 					Start Work
 				</Button>
-				<Button variant="contained" color="error" disabled={true}>
+				<Button
+					variant="contained"
+					color="error"
+					disabled={!startWork}
+					onClick={handleWorkButton}
+				>
 					End Work
 				</Button>
 			</Box>
